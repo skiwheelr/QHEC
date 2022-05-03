acdd8d962258dcba7cb7a446b37d4ccc18b3a39d
skiwheelr <skiwheelr@yahoo.com>
2022-05-02 22:03:32 -0700

    qhec

--- qhec.py
+++ qhec.py
@@ -13,14 +13,15 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.common.exceptions import NoSuchElementException        
 
 driver = webdriver.Chrome()
 driver.set_window_size(1570, 882)
 # Test name: search4qhec
 # Step # | name | target | value
 driver.get("https://potomac.buildingperformance.com/Account/LogOn?returnUrl=%2F")
-inputElement = driver.find_element_by_id("username").send_keys('melissap0303@gmail.com')
-inputElement = driver.find_element_by_id("password").send_keys('')
+inputElement1 = driver.find_element_by_id("username").send_keys('melissap0303@gmail.com')
+inputElement2 = driver.find_element_by_id("password").send_keys('')
 driver.find_element_by_id("loginSubmit").click()
 driver.find_element_by_link_text("Create Job and Validate Customer").click()
 
@@ -29,13 +30,23 @@ reader = csv.reader(file, delimiter=',')
 for row in reader:
     # print(row[0])
     # print(row[1])
-    numElement = driver.find_element_by_id("SERV_ADDR_HOUSE_NUM").send_keys(row[0])
-    addrElement = driver.find_element_by_id("SERV_ADDR_STREET_NAME").send_keys(row[1])
-    driver.find_element(By.CSS_SELECTOR, ".c-btn").click()
-    isqhec = driver.find_element(By.CSS_SELECTOR, ".even:nth-child(12) > td").text
-    print(isqhec)
+    numElement = driver.find_element_by_id("SERV_ADDR_HOUSE_NUM")
+    numElement.clear()
+    numElement.send_keys(row[0])
 
-    time.sleep(4)
+    addrElement = driver.find_element_by_id("SERV_ADDR_STREET_NAME")
+    addrElement.clear()
+    addrElement.send_keys(row[1])
+    driver.find_element_by_css_selector(".c-btn").click()
+    
+    try:
+        isqhec = driver.find_element(By.CSS_SELECTOR, ".even:nth-child(12) > td").text
+        if isqhec == "":
+            print("Good2Call")
+        else:
+            print(isqhec)
+    except NoSuchElementException:
+        print("errorNotFound")
 
 
 
